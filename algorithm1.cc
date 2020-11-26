54ec10f62c04d1f71ca473830a8651386e8d11f6
eric.gregori <eric@gregori>
2020-11-26 06:04:11 +0000

    Ready to test lgorithm1

--- algorithm1.cc
+++ algorithm1.cc
@@ -76,69 +76,74 @@ int main(int argc, char **argv)
 
 int Algorithm(STATE state, LidarBot *robot)
 {
-        printf("\n");
-        if( int points = robot->ReadLidar() )
-        {
- #if 1
-            printf("%d-%d %d-%d %d-%d\n", robot->l_sector_start, robot->l_sector_end,
-                                          robot->f_sector_start, robot->f_sector_end, 
-                                          robot->r_sector_start, robot->r_sector_end);
-            printf("\n%f %f %f %d\n", robot->left.norm, robot->forward.norm, robot->right.norm, robot->direction_of_max_range);
-            printf("%f %f %f\n", robot->left.actual, robot->forward.actual, robot->right.actual);
-            switch(state)
+    SECTOR left, right, forward;
+
+    printf("\n");
+    try
+    {
+        robot->ReadLidarRaw();
+    }
+    catch(...)
+    {
+        printf("Error reading lidar\n");
+    }
+    robot->GetSectors(&left, &forward, &right);
+    printf("\n\n\nnormalized: %f %f %f\n", left.norm, forward.norm, right.norm);
+    printf("raw: %f %f %f\n", left.raw, forward.raw, right.raw);
+    printf("furthest: %f %d\n", robot->max_range, robot->direction_of_max_range);
+    robot->VisualizeRanges();
+    printf("%d-%d %d-%d %d-%d\n", robot->l_sector_start, robot->l_sector_end,
+                                  robot->f_sector_start, robot->f_sector_end, 
+                                  robot->r_sector_start, robot->r_sector_end);
+    switch(state)
+    {
+        case STATE_STOP:
+            printf("stop\n");
+            robot->Move((unsigned char *)"s");
+            break;
+        case STATE_SET_TURN:
+            printf("rotate\n");
+            if(robot->direction_of_max_range < robot->f_sector_start)
+                state = STATE_TURN_LEFT;
+            else
+                state = STATE_TURN_RIGHT;
+            break;
+        case STATE_TURN_LEFT:
+            printf("Turning left\n");
+            robot->Move((unsigned char *)"l");
+
+            if( forward.raw > LOOKS_INTERESTING_ACTUAL )
             {
-                case STATE_STOP:
-                    printf("stop\n");
-                    robot->Move((unsigned char *)"s");
-                    break;
-                case STATE_SET_TURN:
-                    printf("rotate\n");
-                    if(robot->direction_of_max_range < robot->f_sector_start)
-                        state = STATE_TURN_LEFT;
-                    else
-                        state = STATE_TURN_RIGHT;
-                    break;
-                case STATE_TURN_LEFT:
-                    printf("Turning left\n");
-                    robot->Move((unsigned char *)"l");
-
-                    if( robot->forward.actual > LOOKS_INTERESTING_ACTUAL )
-                    {
-                        robot->Move((unsigned char *)"s");
-                        state = STATE_MOVE_FORWARD;
-                    }
-                    break;
-                case STATE_TURN_RIGHT:
-                    printf("Turning right\n");
-                    robot->Move((unsigned char *)"r");
-
-                    if( robot->forward.actual > LOOKS_INTERESTING_ACTUAL )
-                    {
-                        robot->Move((unsigned char *)"s");
-                        state = STATE_MOVE_FORWARD;
-                    }
-                    break;
-                case STATE_MOVE_FORWARD:
-                    // LM(0-9) = 4.5*F+4.5*R    RM(0-9) = 4.5*F+4.5*L
-                    int speed[] = {'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-                    unsigned char m[3];
-
-                    printf("forward ");
-                    m[0] = 'm';
-                    m[1] = speed[(int)(7.0*robot->forward.norm+7.0*robot->right.norm)];
-                    m[2] = speed[(int)(7.0*robot->forward.norm+7.0*robot->left.norm)];
-                    printf(" %c %c\n", m[1], m[2]);
-                    robot->Move(m);
-                    if(robot->right.actual < TOO_CLOSE_ACTUAL || robot->forward.actual < TOO_CLOSE_ACTUAL || robot->left.actual < TOO_CLOSE_ACTUAL)
-                    {
-                        robot->Move((unsigned char *)"s");
-                        state = STATE_SET_TURN;
-                    }
-                    break;
+                robot->Move((unsigned char *)"s");
+                state = STATE_MOVE_FORWARD;
             }
-#endif
-#if 1
-            robot->VisualizeRanges();
-#endif
-        }
+            break;
+        case STATE_TURN_RIGHT:
+            printf("Turning right\n");
+            robot->Move((unsigned char *)"r");
+
+            if( forward.raw > LOOKS_INTERESTING_ACTUAL )
+            {
+                robot->Move((unsigned char *)"s");
+                state = STATE_MOVE_FORWARD;
+            }
+            break;
+        case STATE_MOVE_FORWARD:
+            // LM(0-9) = 4.5*F+4.5*R    RM(0-9) = 4.5*F+4.5*L
+            int speed[] = {'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+            unsigned char m[3];
+
+            printf("forward ");
+            m[0] = 'm';
+            m[1] = speed[(int)(7.0*forward.norm+7.0*right.norm)];
+            m[2] = speed[(int)(7.0*forward.norm+7.0*left.norm)];
+            printf(" %c %c\n", m[1], m[2]);
+            robot->Move(m);
+            if(right.raw < TOO_CLOSE_ACTUAL || forward.raw < TOO_CLOSE_ACTUAL || left.raw < TOO_CLOSE_ACTUAL)
+            {
+                robot->Move((unsigned char *)"s");
+                state = STATE_SET_TURN;
+            }
+            break;
+    } // switch
 }
